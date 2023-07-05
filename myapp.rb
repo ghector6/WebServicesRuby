f5ff82a89c8735055133393af4342f978751bc80
Hector Garcia Rosales <ghectormus@gmail.com>
2023-07-05 14:15:42 -0600

    Add: myapp

---
+++ myapp.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+
+get '/employees' do
+end
+
+get '/employees/:id' do
+end
+
+post '/employees' do
+end
+
+put '/employees' do
+end
+
+delete '/employees' do
+end
+
+patch '/employees' do
+end
